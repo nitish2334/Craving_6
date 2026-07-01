473396e60f3da8ab12dad46173b91a300d6745e3
nitish2334 <raunaksinha1803@gmail.com>
2026-07-01 23:54:13 +0530

    backend

--- CLIENT/src/App.jsx
+++ CLIENT/src/App.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import ContactUs from "./pages/ContactUs";
+import {Toaster} from "react-hot-toast";
+import UserDashboard from "./Pages/dashboard/userDashboard";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
+      <Toaster/>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+{/* dashboard route */}
+<Route>
+
+</Route>
+
         </Routes>
       </BrowserRouter>
     </>
--- CLIENT/src/Pages/Home.jsx
+++ CLIENT/src/Pages/Home.jsx
@@ -3,8 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <>
-      <div className="bg-black
-      -300">Home</div>
+      <div className="bg-red-300">Home</div>
     </>
   );
 };
